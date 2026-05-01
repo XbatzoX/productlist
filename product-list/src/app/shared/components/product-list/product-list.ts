feebe0c0c11dd7fcdd272af3257bd51123d895c7
BEN <benbasic@outlook.de>
2026-05-01 15:20:03 +0200

    added list with control flow

--- product-list/src/app/shared/components/product-list/product-list.ts
+++ product-list/src/app/shared/components/product-list/product-list.ts
@@ -13,7 +13,7 @@ export class ProductList {
     "description": "Eine ergonomische Gaming-Maus mit hoher Präzision und einstellbarer DPI. Ideal für FPS- und MOBA-Spiele, bietet sie eine langlebige Bauweise und komfortable Seitentasten für schnelles Reagieren.",
     "specs": "dpi: 6400, cable length: 1.8m, color: Schwarz",
     "stock": 120,
-    "price": 29.99
+    "price": 2500000
   },
   {
     "name": "USB-C Kabel",
