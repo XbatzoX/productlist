a548fbd3446350df426af40b47b74bd13b71f78d
BEN <benbasic@outlook.de>
2026-05-03 08:12:17 +0200

    added function to service

--- product-list/src/app/shared/components/product-detail/product-detail.ts
+++ product-list/src/app/shared/components/product-detail/product-detail.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Products } from '../../services/products';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,17 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetail {
 
   private route = inject(ActivatedRoute);
+  productService = inject(Products);
+
   ngOnInit(){
     let currentName = this.route.snapshot.paramMap.get('name');
-    if(currentName){this.detail.name = currentName;}
+    if(currentName){this.productService.setProductDetailByName(currentName);}
+    this.detail = this.productService.productdetail;
   }
 
   detail = {
-    "name": "Gaming Maus",
-    "description": "Eine ergonomische Gaming-Maus mit hoher Präzision und einstellbarer DPI. Ideal für FPS- und MOBA-Spiele, bietet sie eine langlebige Bauweise und komfortable Seitentasten für schnelles Reagieren.",
-    "specs": "dpi: 6400, cable length: 1.8m, color: Schwarz",
-    "stock": 120,
-    "price": 29.99
+    "name": "n/a",
+    "description": "n/a",
+    "specs": "n/a",
+    "stock": 0,
+    "price": 0
   }
 
   deleteDetail():void{
