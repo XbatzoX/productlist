af8e27f44c9ec075d33992d255a24ee906537cfc
BEN <benbasic@outlook.de>
2026-05-10 15:21:54 +0200

    added submit function for form

--- product-list/src/app/shared/components/product-form/product-form.ts
+++ product-list/src/app/shared/components/product-form/product-form.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-form.html',
   styleUrl: './product-form.scss',
 })
-export class ProductForm {}
+export class ProductForm {
+  productForm = new FormGroup({
+    name: new FormControl('n/a'),
+    description: new FormControl('n/a'),
+    stock: new FormControl(0),
+    price: new FormControl(0.00)
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.productForm.value);
+  }
+}
